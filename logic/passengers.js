f0f4079d4626863f1654cf3a36393880f606d528
Hunter Long <hunterlong@admins-MacBook-Pro.local>
2020-10-26 17:42:32 -0500

    finished 5

--- logic/passengers.js
+++ logic/passengers.js
@@ -13,7 +13,72 @@ function Passengers() {
         }
     }
 
-    return {checkFlightCapacity};
+    function distributeAllSeatsToAllPassengers(numVIP, numReg, numFlights, numBusSeats, numEconSeats) {
+        var seating = {
+            vipPassengersWithBusinessSeats: 0,
+            vipPassengersWithEconomySeats: 0,
+            regularPassengersWithBusinessSeats: 0,
+            regularPassengersWithEconomySeats: 0
+        }
+        let totalNumBusSeats = numBusSeats * numFlights;
+        let totalNumEconSeats = numEconSeats * numFlights;
+
+        //test to see if there are more vip or bus seats and assigns correctly
+        if(numVIP >= totalNumBusSeats){
+            seating.vipPassengersWithBusinessSeats = totalNumBusSeats;
+            
+            numVIP -= totalNumBusSeats;
+            totalNumBusSeats = 0;
+        } else {
+            seating.vipPassengersWithBusinessSeats = numVIP;
+            totalNumBusSeats -= numVIP;
+            numVIP = 0;
+        }
+
+        if(numVIP > 0) {
+            if(numVIP >= totalNumEconSeats) {
+                seating.vipPassengersWithEconomySeats = totalNumEconSeats;
+                
+                numVIP -= totalNumEconSeats;
+                totalNumEconSeats = 0;
+            } else {
+                seating.vipPassengersWithEconomySeats = numVIP;
+                totalNumEconSeats -= numVIP;
+                numVIP = 0;
+            }
+        }
+
+        if(totalNumBusSeats > 0) {
+            if(numReg >= totalNumBusSeats) {
+                seating.regularPassengersWithBusinessSeats = totalNumBusSeats;
+                numReg -= totalNumBusSeats;
+                totalNumBusSeats = 0;
+            } else {
+                seating.regularPassengersWithBusinessSeats = numReg;
+                totalNumBusSeats -= numReg;
+                numReg = 0;
+            }
+        }
+
+        if(totalNumEconSeats > 0) {
+            if(numReg >= totalNumEconSeats) {
+                seating.regularPassengersWithEconomySeats = totalNumEconSeats;
+                
+                numReg -= totalNumEconSeats;
+                totalNumEconSeats = 0;
+            } else {
+                seating.regularPassengersWithEconomySeats = numReg;
+                totalNumEconSeats -= numReg;
+                numReg = 0;
+            }
+        }
+
+
+        return seating;
+
+    }
+
+    return {checkFlightCapacity, distributeAllSeatsToAllPassengers};
 }
 
 module.exports = Passengers();
