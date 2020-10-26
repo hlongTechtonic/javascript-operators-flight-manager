0300dc5a9faef3bf563fab9b98350ce3422def41
Hunter Long <hunterlong@admins-MacBook-Pro.local>
2020-10-26 16:23:13 -0500

    finished 4

--- logic/passengers.js
+++ logic/passengers.js
@@ -1,4 +1,20 @@
 function Passengers() {
+    function checkFlightCapacity(flightCap, passArr) {
+        let total = 0;
+        passArr.forEach(element => {
+            total += element;
+        });
 
+        if(total < flightCap) {
+            return total;
+        }
+        if(total > flightCap) {
+            throw new Error("you are over the flight capacity");
+        }
+    }
+
+    return {checkFlightCapacity};
 }
+
+module.exports = Passengers();
 
