b2c05081b4abceef9d62b3974095313d0ce43d0f
Hunter Long <hunterlong@admins-MacBook-Pro.local>
2020-10-26 17:52:47 -0500

    finished manage flight and passengers

--- logic/util.js
+++ logic/util.js
@@ -1,4 +1,27 @@
 function Util() {
+    function calculateTotalDistributedPassengers(seating) {
+        let total = 0;
+        
+        for (param in seating) {
+            total += seating[param];
+        }
+
+        return total;
+    }
+
+    function calculateTotalNumberOfPassengers(arr) {
+        let total = 0;
+
+        arr.forEach(element => {
+            total += element;
+        });
+
+        return total;
+    }
+
+    return {calculateTotalDistributedPassengers, calculateTotalNumberOfPassengers};
 
 }
+
+module.exports = Util();
 
